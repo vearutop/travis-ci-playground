1275d2121125c65d489e8f188eca9f8d9ed878ef
Viacheslav Poturaev <vearutop@gmail.com>
2015-05-05 10:04:56 +0700

    invoke testing

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -5,6 +5,5 @@ class SampleTest extends PHPUnit_Framework_TestCase {
         var_dump(phpversion());
         var_dump(file_get_contents('http://127.0.0.1:8000/'));
         var_dump(file_get_contents('http://127.0.0.1:1337/'));
-
     }
 }
