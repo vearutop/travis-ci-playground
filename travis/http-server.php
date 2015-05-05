43967147482aef75d2d163c6402e58ea4b4b092d
Viacheslav Poturaev <vearutop@gmail.com>
2015-05-05 10:43:25 +0700

    invoke testing

--- travis/http-server.php
+++ travis/http-server.php
@@ -1,14 +1,18 @@
 <?php
 
+print_r($_SERVER);
+
+/*
 if (empty($_COOKIE['coo'])) {
     $_COOKIE['coo'] = 0;
     setcookie('coo', 1);
-    header("Location: /");
+    header("Location: /1");
 }
 elseif (1 == $_COOKIE['coo']) {
     setcookie('coo', 2);
-    header("Location: /");
+    header("Location: /2");
 }
 
 echo $_COOKIE['coo'];
 
+*/
