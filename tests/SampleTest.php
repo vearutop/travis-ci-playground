6e0e2547944039b3e25ee678fcb7e1ef3d756759
Viacheslav Poturaev <vearutop@gmail.com>
2015-05-05 10:39:19 +0700

    invoke testing

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -3,7 +3,7 @@
 class SampleTest extends PHPUnit_Framework_TestCase {
     public function testOne() {
         var_dump(phpversion());
-        var_dump(file_get_contents('http://127.0.0.1:8000/'));
-        var_dump(file_get_contents('http://127.0.0.1:1337/'));
+        var_dump(file_get_contents('http://localhost:8000/'));
+        var_dump(file_get_contents('http://localhost:1337/'));
     }
 }
