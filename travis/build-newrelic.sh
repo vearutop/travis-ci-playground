38edd8d04dc4746a6145e54bac131b3677dcd31f
Viacheslav Poturaev <vearutop@gmail.com>
2015-05-28 11:13:27 +0700

    newrelic disabled

--- travis/build-newrelic.sh
+++ travis/build-newrelic.sh
@@ -4,8 +4,8 @@ mkdir newrelic
 cd newrelic
 wget https://download.newrelic.com/agent_sdk/nr_agent_sdk-v0.16.1.0-beta.x86_64.tar.gz
 tar -xzf nr_agent_sdk-*.tar.gz
-#sudo cp nr_agent_sdk-*/include/* /usr/local/include/
-#sudo cp nr_agent_sdk-*/lib/* /usr/local/lib/
+sudo cp nr_agent_sdk-*/include/* /usr/local/include/
+sudo cp nr_agent_sdk-*/lib/* /usr/local/lib/
 
 git clone https://github.com/chregu/hhvm-newrelic-ext.git
 cd hhvm-newrelic-ext
