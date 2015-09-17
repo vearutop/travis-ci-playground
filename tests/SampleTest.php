005dd065a37dbff85fd65239f6e7d3527bdfdcc5
Viacheslav Poturaev <nanopeni@gmail.com>
2015-09-17 21:10:58 +0700

    test sort

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -3,6 +3,17 @@
 class SampleTest extends PHPUnit_Framework_TestCase {
     public function testSort() {
         $a = array(10 => 0, 1, 2, 1, 3, 0, 1, 1, 0);
-        var_dump($a);
+        asort($a);
+        $this->assertSame(array (
+            15 => 0,
+            10 => 0,
+            18 => 0,
+            16 => 1,
+            17 => 1,
+            13 => 1,
+            11 => 1,
+            12 => 2,
+            14 => 3,
+        ), $a);
     }
 }
